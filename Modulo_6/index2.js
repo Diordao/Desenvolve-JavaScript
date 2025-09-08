cc4a313e6b50e58bcbf5c92f5e1cd1d88c7c4132
Diordao <diordovilela@gmail.com>
2025-09-08 10:43:05 -0300

    Add files via upload
    Exercício - Seletores 2

---
+++ Modulo_6/index2.js
@@ -0,0 +1,37 @@
+let curtidas = [];
+
+const inputNome = document.getElementById("nome");
+const botaoCurtir = document.getElementById("btn-curtir");
+const resultado = document.getElementById("resultado");
+
+function atualizarMensagem() {
+  if (curtidas.length === 0) {
+    resultado.textContent = "Ninguém curtiu";
+  } else if (curtidas.length === 1) {
+    resultado.textContent = `${curtidas[0]} curtiu`;
+  } else if (curtidas.length === 2) {
+    resultado.textContent = `${curtidas[0]} e ${curtidas[1]} curtiram`;
+  } else {
+    resultado.textContent = `${curtidas[0]}, ${curtidas[1]} e mais ${curtidas.length - 2} pessoas curtiram`;
+  }
+}
+
+function curtir() {
+  const nome = inputNome.value.trim();
+
+  if (nome === "") {
+    alert("Digite um nome antes de curtir!");
+    return;
+  }
+
+ if (!curtidas.includes(nome)) {
+    curtidas.push(nome);
+  } else {
+    alert(`${nome} já curtiu!`);
+  }
+
+  inputNome.value = "";
+  atualizarMensagem();
+}
+
+botaoCurtir.addEventListener("click", curtir);
